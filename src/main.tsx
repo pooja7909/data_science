f4afbd62d4ef6c7a6634b05945d2784017dacc21
pooja7909 <52460015+pooja7909@users.noreply.github.com>
2026-04-08 19:53:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import { PasswordGate } from './components/PasswordGate';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PasswordGate>
-      <App />
-    </PasswordGate>
+    <App />
   </StrictMode>,
 );
